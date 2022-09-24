defa3775596ec870e1b6d482794569d18dca4e19
Kulakov D.A., ISTb-21-1 <dragonipam@yandex.ru>
2022-09-24 20:00:09 +0800

    add function system of numbers for writing label and add random number and system in label

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,34 +30,57 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(171, 172);
+            this.button1.Location = new System.Drawing.Point(150, 135);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(223, 36);
+            this.button1.Size = new System.Drawing.Size(320, 47);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Проверка";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(610, 52);
+            this.label1.Location = new System.Drawing.Point(31, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.Size = new System.Drawing.Size(439, 79);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = " label";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(150, 102);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(320, 27);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(556, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Баллы:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(624, 225);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -68,5 +91,7 @@
 
         private Button button1;
         private Label label1;
+        private TextBox textBox1;
+        private Label label2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,45 @@ namespace taskformother
         public Form1()
         {
             InitializeComponent();
+            Random r = new Random();
+            int Number = r.Next(1, 999);
+            int NS = r.Next(1, 4);
+            int AntiNS = r.Next(1, 4);
+            while (AntiNS == NS)
+            {
+                AntiNS = r.Next(1, 4);
+            }
+            label1.Text = ($"Переведите число {Number} из {NumberOfSystem(NS)}-ой системы счисления " +
+                $" в {NumberOfSystem(AntiNS)}-ую систему счисления");
+        }
+        public static int NumberOfSystem(int NS)
+        {
+            int outSystem = 0;
+            switch (NS)
+            {
+                case 1:
+                    outSystem = 2;
+                    break;
+                case 2:
+                    outSystem = 8;
+                    break;
+                case 3:
+                    outSystem = 10;
+                    break;
+                default:
+                    outSystem = 16;
+                    break;
+            }
+            return outSystem;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
